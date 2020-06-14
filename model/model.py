a610542551b4bcfd092b5ce1772f577ed84d69d1
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2020-06-14 04:25:50 +0200

    Init file for model.py

--- model/model.py
+++ model/model.py
@@ -1,3 +1,16 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
+
+class CharRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers):
+        super(CharRNN, self).__init__()
+        self.num_layers = num_layers
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+
+        self.rnn = nn.GRU(input_size=self.input_size,
+                          hidden_size=self.hidden_size,
+                          num_layers=self.num_layers)
