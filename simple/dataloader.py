92cb542022ece6f3b5035e3820c743e1cb8ad8de
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2020-06-17 00:18:28 +0200

    Changed batch

--- simple/dataloader.py
+++ simple/dataloader.py
@@ -7,8 +7,9 @@ from torch.utils.data import Dataset, DataLoader
 class TextDataset(Dataset):
     # Dostoyevsky: Brothers Karamazovy
     def __init__(self, link='https://www.gutenberg.org/files/28054/28054-0.txt',
-                 batch_size=48,
-                 sequence_length=32):
+                 batch_size = 48,
+                 batch_num = 3000,
+                 sequence_length = 32):
         self.raw = request.urlopen(link).read().decode('utf8').lower()
         self.tokens = [c for c in self.raw]
         self.chars = sorted(set(self.tokens))
@@ -16,14 +17,23 @@ class TextDataset(Dataset):
         self.char2inx = {ch: ii for ii, ch in self.inx2char.items()}
         self.encoded = [self.char2inx[inx] for inx in self.tokens]
 
-        # self.batch_size = batch_size
-        # self.sequence_length = sequence_length
+        self.batch_size = batch_size
+        self.batch_num = batch_num
+        self.sequence_length = sequence_length
         # self.batch_num = len(self.encoded) // (self.batch_size * self.sequence_length)
         # self.encoded2arr = np.array(self.encoded[:self.batch_size * self.sequence_length * self.batch_num])
         # self.encoded2arr = self.encoded2arr.resize([self.batch_num * self.batch_size, self.sequence_length])
 
     def __len__(self):
-        return self.batch_num * self.batch_size
+        return self.batch_num
 
     def __getitem__(self, idx):
-        start_ = np.random
+        batch_input = torch.LongTensor(self.batch_size, self.sequence_length)
+        batch_output = torch.LongTensor(self.batch_size, self.sequence_length)
+
+        for i in range(batch_input.shape[0]):
+            start_idx = np.random.randint(0, len(self.encoded)-1-self.sequence_length)
+            end_idx = start_idx + self.sequence_length
+            batch_input[i] = torch.tensor(self.encoded[start_idx:end_idx])
+            batch_output[i] = torch.tensor(self.encoded[start_idx+1:end_idx+1])
+        return batch_input, batch_output
